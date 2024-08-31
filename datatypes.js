b7abd2d0be6d45c7cacd89f7edf0946a9ac4766b
Suprim Khatri <supremekhatri62@gmail.com>
2024-08-31 08:43:26 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// alert('Hello');
+let name = ' Suprim';
+let age = 19;
+let isLoggedIn = true;
---
+++ variable.js
@@ -0,0 +1,5 @@
+const name = 'slash';
+let email = 'slash77@google.com';
+let address = 'Butwal';
+
+console.table([name, email , address]);
