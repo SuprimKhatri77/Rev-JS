1b2326b4aa59f8aebc085db8bd127cc4055f68f2
Suprim Khatri <supremekhatri62@gmail.com>
2025-03-05 17:03:11 +0545

    arrow functions and this

---
+++ revision/functions/03.js
@@ -0,0 +1,25 @@
+// arrow functions and this
+
+const obj = {
+    username: 'Slash',
+    price: 999,
+    welcomeMessage:function (){
+        console.log(`${this.username}, welcome!`);
+        
+    }
+}
+
+// obj.welcomeMessage()
+
+
+const arrowFunction = ()=>{
+    console.log('arrow function');
+    
+}
+
+// arrowFunction()
+
+// implicit return
+
+const func = (num1,num2) => (num1+num2)
+console.log(func(5,2))
